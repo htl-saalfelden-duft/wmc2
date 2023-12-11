b2b1d6f7de99fa54c529739991336359e7a7d725
Dietmar Duft <dietmar@duft.it>
2023-12-11 15:36:00 +0100

    Added arduino

--- projects/12_js_bulb/02_progress/app10.js
+++ projects/12_js_bulb/02_progress/app10.js
@@ -3,20 +3,30 @@ var lampeAlert = document.getElementById('lampeAlert')
 var container = document.querySelector(".container")
 
 function bulb(status) {
+    if(status == 'on') {
+        myImage.src='./images/pic_bulbon.gif'
+        showAlert('on')
+    }
+    if(status == 'off') {
+        myImage.src='./images/pic_bulboff.gif'
+        showAlert('off')
+    }
+}
+
+function showAlert(status) {
     var alert = document.createElement('div')
     alert.classList.add('alert')
     alert.classList.add('mt-4')
 
     if(status == 'on') {
-        myImage.src='./images/pic_bulbon.gif'
         alert.classList.add('alert-warning')
         alert.innerHTML = "Die Lampe ist an"
     }
     if(status == 'off') {
-        myImage.src='./images/pic_bulboff.gif'
         alert.classList.add('alert-primary')
         alert.innerHTML = "Die Lampe ist aus"
     }
+
 
     container.appendChild(alert)
 
@@ -26,6 +36,6 @@ function bulb(status) {
 }
 
 
-/* <div id="lampeAlert" class="alert alert-primary mt-4" role="alert">
+{/* <div id="lampeAlert" class="alert alert-primary mt-4" role="alert">
     Die Lampe ist aus
-</div> */
+</div> */}
