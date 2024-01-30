e3b8c849b74c731530ef0729651e9c37387d7749
dduft <dietmar@duft.it>
2024-01-30 15:55:47 +0100

    bulb progress

--- projects/12_js_bulb/02_progress/app11.js
+++ projects/12_js_bulb/02_progress/app11.js
@@ -1,6 +1,5 @@
-var myImage = document.getElementById('myImage')
-var lampeAlert = document.getElementById('lampeAlert')
-var container = document.querySelector(".container")
+import { bulb, showAlert } from "./ui11.js"
+
 var btnOn = document.querySelector('#btnOn')
 var btnOff = document.querySelector('#btnOff')
 
@@ -8,50 +7,14 @@ var url = "http://10.10.1.83"
 
 btnOn.addEventListener('click', async function() {
     bulb('on')
-    showAlert('on')
+    zeigeAlarm('on')
 
     await fetch(`${url}?led=on`)
 })
 
 btnOff.addEventListener('click', async function() {
     bulb('off')
-    showAlert('off')
+    zeigeAlarm('off')
 
     await fetch(`${url}?led=off`)
 })
-
-function bulb(status) {
-    if(status == 'on') {
-        myImage.src='./images/pic_bulbon.gif'
-    }
-    if(status == 'off') {
-        myImage.src='./images/pic_bulboff.gif'
-    }
-}
-
-function showAlert(status) {
-    var alert = document.createElement('div')
-    alert.classList.add('alert')
-    alert.classList.add('mt-4')
-
-    if(status == 'on') {
-        alert.classList.add('alert-warning')
-        alert.innerHTML = "Die Lampe ist an"
-    }
-    if(status == 'off') {
-        alert.classList.add('alert-primary')
-        alert.innerHTML = "Die Lampe ist aus"
-    }
-
-
-    container.appendChild(alert)
-
-    setTimeout(function() {
-        alert.remove()
-    }, 2000)
-}
-
-
-/* <div id="lampeAlert" class="alert alert-primary mt-4" role="alert">
-    Die Lampe ist aus
-</div> */
--- projects/15_jsonToHtml/app.js
+++ projects/15_jsonToHtml/app.js
@@ -5,9 +5,19 @@ const proceed = () => {
     .then(response => response.json())
     .then(users => {
         users.forEach(user => {
+            console.log(user)
+
             const tr = document.createElement('tr')
+
             tr.innerHTML = `<td>${user.id}</td>
-                            <td>${user.name}</td>`
+                            <td>${user.name}</td>
+                            <td>${user.email}</td>
+                            <td>${user.address.city}</td>
+                            <td>
+                                <button class="btn btn-outline-primary"><i class="bi bi-pencil"></i></button>
+                                <button class="btn btn-outline-danger"><i class="bi bi-x-lg"></i>
+                                </button>
+                            </td>`
             usersElement.appendChild(tr)
         });
 
